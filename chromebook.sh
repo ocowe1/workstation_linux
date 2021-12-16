9f322aac58bcd5647e722e39ca0007d5ba7f5c8f
Vinicius Costa <53479130+ocowe1@users.noreply.github.com>
2021-12-16 11:01:08 -0300

    Update and rename pos-instalacao.sh to chromebook.sh

---
+++ chromebook.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+sudo rm /var/lib/dpkg/lock-frontend ; sudo rm var/cache/apt/archives/lock ;
+
+sudo apt update ; sudo apt upgrade ;
+
+PPA_LIBRATBAG="ppa:libratbag-piper/piper-libratbag-git"
+PPA_LUTRIS="ppa:lutris-team/lutris"
+PPA_GRAPHICS_DRIVERS="ppa:graphics-drivers/ppa"
+
+URL_WINE_KEY="https://dl.winehq.org/wine-builds/winehq.key"
+URL_PPA_WINE="https://dl.winehq.org/wine-builds/ubuntu/"
+URL_4K_VIDEO_DOWNLOADER="https://dl.4kdownload.com/app/4kvideodownloader_4.9.2-1_amd64.deb"
+URL_INSYNC="https://d2t3ff60b2tol4.cloudfront.net/builds/insync_3.0.20.40428-bionic_amd64.deb"
+
+DIRETORIO_DOWNLOADS="$HOME"
+
+## Adicionando/Confirmando arquitetura de 32 bits ##
+sudo dpkg --add-architecture i386
+
+## Atualizando o repositório ##
+sudo apt update -y
+
+## Adicionando repositórios de terceiros e suporte a Snap (Driver Logitech, Lutris e Drivers Nvidia)##
+sudo apt-add-repository "$PPA_LIBRATBAG" -y
+sudo add-apt-repository "$PPA_LUTRIS" -y
+sudo apt-add-repository "$PPA_GRAPHICS_DRIVERS" -y
+wget -nc "$URL_WINE_KEY"
+sudo apt-key add winehq.key
+sudo apt-add-repository "deb $URL_PPA_WINE bionic main"
+# ---------------------------------------------------------------------- #
+
+# ----------------------------- EXECUÇÃO ----------------------------- #
+## Atualizando o repositório depois da adição de novos repositórios ##
+sudo apt update -y
+
+## Download e instalaçao de programas externos ##
+wget -c "$URL_4K_VIDEO_DOWNLOADER"
+wget -c "$URL_INSYNC"            
+
+## Instalando pacotes .deb baixados na sessão anterior ##
+sudo dpkg -i *.deb
+
+# Instalar programas no apt
+for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
+  if ! dpkg -l | grep -q $nome_do_programa; then # Só instala se já não estiver instalado
+    apt install "$nome_do_programa" -y
+  else
+    echo "[INSTALADO] - $nome_do_programa"
+  fi
+done
+
+sudo apt install --install-recommends winehq-stable wine-stable wine-stable-i386 wine-stable-amd64 -y
+
+## Instalando pacotes Flatpak ##
+flatpak install flathub com.obsproject.Studio -y
+
+## Instalando pacotes Snap ##
+sudo snap install spotify
+sudo snap install skype --classic
+# ---------------------------------------------------------------------- #
+
+#loja
+sudo apt update - look for software / system updates ;
+sudo apt dist-upgrade - upgrade your system files ;
+sudo apt-get install gnome-software gnome-packagekit ;
+sudo apt install plasma-discover ;
+
+#steam
+sudo dpkg --add-architecture i386 ;
+sudo apt update ;
+sudo apt install wget gdebi-core libgl1-mesa-glx:i386 ;
+sudo wget -O ~/steam.deb http://media.steampowered.com/client/installer/steam.deb ;
+sudo gdebi ~/steam.deb ;
+
+# ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
+## Finalização, atualização e limpeza##
+sudo apt update && sudo apt dist-upgrade -y
+flatpak update
+sudo apt autoclean
+sudo apt autoremove -y
